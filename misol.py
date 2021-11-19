e26f89f68f799566b77b872a4f053fd43d23b8c7
Avazbek Shukurjonov <avazbek.shukurjonov@bk.ru>
2021-11-19 23:08:30 +0500

    Added some file

---
+++ misol.py
@@ -0,0 +1,38 @@
+# with  open('abc.txt') as abc:
+#     with open('abcd.txt', 'a') as abcd:
+#         for line in abc 
+
+
+# with open('dog.jpeg', 'rb') as img:
+#     print(img.read())
+
+
+
+# with open('dog.jpg', 'rb') as img:
+#     with open('dog_copy.jpg', 'wb') as copy:
+#         for line in img:
+#             copy.write(line)
+
+
+# import os
+# os.rename('hello.py', 'bye.py')
+
+
+
+#print(os.path.isdir('img'))
+# print(os.path.isdir('img'))
+
+# from datetime import datetime
+# with open('bye.txt', 'w') as abc:
+#     login=input('login kiriting  ')
+#     parol=input('parolni kiriting  ')
+#     i=0
+#     while True:
+#         print(login, parol)
+#         print(datetime.now)
+
+
+
+# login=input('login kiriting  ')
+# parol=input('parolni kiriting  ')
+# with open('bye.txt', 'w') as abc:
